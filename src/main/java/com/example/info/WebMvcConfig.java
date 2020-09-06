d0e7e32061e8d65f4b4a3e7ad2b7fde7afc9cc86
zorba <forthezorba@gmail.comgit config --global user.email forthezorba@gmail.comgit config --global user.email forthezorba@gmail.com>
2020-09-06 18:13:44 +0900

    pic

--- src/main/java/com/example/info/WebMvcConfig.java
+++ src/main/java/com/example/info/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/summernoteImage/**")
-                .addResourceLocations("/image/upload/");
+                .addResourceLocations("file:///image/upload/");
     }
 }
